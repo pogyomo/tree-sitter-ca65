ab4a96c84ecaee23cf1769367584bd6581ba7891
pogyomo <pogyomo@gmail.com>
2025-08-16 14:58:34 +0900

    feat(query): indent source_line outside block

--- queries/indents.scm
+++ queries/indents.scm
@@ -1,14 +1,14 @@
 [
-(pseudo_inst_struct
-  (dot_keyword_struct))
-(pseudo_inst_union
-  (dot_keyword_union))
-(pseudo_inst_proc
-  (dot_keyword_proc))
-(pseudo_inst_macro
-  (dot_keyword_macro))
-(pseudo_inst_scope
-  (dot_keyword_scope))
+ (pseudo_inst_struct
+   (dot_keyword_struct))
+ (pseudo_inst_union
+   (dot_keyword_union))
+ (pseudo_inst_proc
+   (dot_keyword_proc))
+ (pseudo_inst_macro
+   (dot_keyword_macro))
+ (pseudo_inst_scope
+   (dot_keyword_scope))
 ] @indent.begin
 
 [
@@ -17,3 +17,5 @@
  (dot_keyword_endproc)
  (dot_keyword_endscope)
 ] @indent.end
+
+(source_line) @indent.dedent
